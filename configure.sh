327f1c423c97fe15d456004bb6d78d6b0a78f4ca
Rolf <rolf.lobker@gmail.com>
2024-12-08 23:11:43 +0100

    ansible: install if not available already

--- configure.sh
+++ configure.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+if ! command -v ansible &>/dev/null; then
+	echo "Ansible needs to be installed."
+	sudo dnf install -y ansible
+fi
+
 ansible-playbook playbook.yaml --tags fonts
