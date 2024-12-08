1d98d9d69a47e12942b9ed5f062a2cdf6aa89908
Rolf <rolf.lobker@gmail.com>
2024-12-08 23:39:50 +0100

    ansible: dnf5 dep not installed by default on fedora 41

--- configure.sh
+++ configure.sh
@@ -4,4 +4,9 @@ if ! command -v ansible &>/dev/null; then
 	sudo dnf install -y ansible
 fi
 
+if [ ! -d "/usr/lib64/python3.13/site-packages/libdnf5/" ]; then
+	echo "Missing python3-libdnf5, installing..."
+	sudo dnf install -y "python3-libdnf5"
+fi
+
 ansible-playbook playbook.yaml -K --tags fonts,packages
